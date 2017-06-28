d139be977f96b8e5b81b173178d11e3d52ec6a68
tapps <t@tapps.email>
2017-06-28 17:16:05 -0500

    adding blog templates

---
+++ home.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @package WordPress
+ * @subpackage bfa
+ * @Best Friends Approach
+ */
+get_header(); ?>
+
+			<div class="content">
+				<h2 class="page-title"><?php single_post_title(); ?></h2>
+				<?php  get_template_part( 'loop', 'list' ); ?>
+			</div><!--/content-->
+	</div><!--/left-zone-->
+	<div id="right-zone">
+		<?php get_sidebar(); ?>
+	</div><!--/rightZone-->
+<?php get_footer(); ?>
---
+++ loop-list.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * @package WordPress
+ * @subpackage bfa
+ * @Best Friends Approach
+ */
+?>
+
+<?php /* Display navigation to next/previous pages when applicable */ ?>
+<?php if ( $wp_query->max_num_pages > 1 ) : ?>
+	<div id="nav-above" class="navigation">
+		<div class="nav-previous alignleft"><?php next_posts_link( __( '<span class="meta-nav">&laquo;</span> Older posts', 'twentyten' ) ); ?></div>
+		<div class="nav-next alignright"><?php previous_posts_link( __( 'Newer posts <span class="meta-nav">&raquo;</span>', 'twentyten' ) ); ?></div>
+	</div><!-- #nav-above -->
+<?php endif; ?>
+
+<?php /* If there are no posts to display, such as an empty archive page */ ?>
+<?php if ( ! have_posts() ) : ?>
+	<article id="post-0" class="post error404 not-found">
+		<h4 class="entry-title"><?php _e( 'Not Found', 'twentyten' ); ?></h4>
+		<div class="entry-content">
+			<p><?php _e( 'Apologies, but no results were found for the requested archive. Perhaps searching will help find a related post.', 'twentyten' ); ?></p>
+			<?php get_search_form(); ?>
+		</div><!-- .entry-content -->
+	</article><!-- #post-0 -->
+<?php endif; ?>
+
+<?php while ( have_posts() ) : the_post(); ?>
+
+
+
+		<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+			<h4 class="entry-title"><a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 'bfa' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"><?php the_title(); ?></a></h4>
+
+			<div class="entry-meta">
+				<?php bfa_posted_on(); ?>
+			</div><!-- .entry-meta -->
+
+	<?php if ( is_archive() || is_search() ) : // Only display excerpts for archives and search. ?>
+			<div class="entry-summary">
+				<?php the_excerpt( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'bfa' ) ); ?>
+			</div><!-- .entry-summary -->
+	<?php else : ?>
+			<div class="entry-content">
+				<?php the_excerpt(); ?>
+				<?php wp_link_pages( array( 'before' => '<div class="page-link">' . __( 'Pages:', 'bfa' ), 'after' => '</div>' ) ); ?>
+			</div><!-- .entry-content -->
+	<?php endif; ?>
+
+			<div class="entry-utility">
+				<?php bfa_posted_in(); ?>
+			</div><!-- .entry-utility -->
+		</article><!-- #post-## -->
+
+
+
+<?php endwhile; // End the loop. Whew. ?>
+
+<?php /* Display navigation to next/previous pages when applicable */ ?>
+<?php if (  $wp_query->max_num_pages > 1 ) : ?>
+				<div id="nav-below" class="navigation">
+					<div class="nav-previous alignleft"><?php next_posts_link( __( '<span class="meta-nav">&laquo;</span> Older posts', 'bfa' ) ); ?></div>
+					<div class="nav-next alignright"><?php previous_posts_link( __( 'Newer posts <span class="meta-nav">&raquo;</span>', 'bfa' ) ); ?></div>
+				</div><!-- #nav-below -->
+<?php endif; ?>
