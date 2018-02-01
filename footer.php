bc1e475f2c53265a094165bb1a55a804f69d1e67
tapps <t@tapps.email>
2018-02-01 14:10:00 -0600

    google analytics code

--- footer.php
+++ footer.php
@@ -18,5 +18,16 @@
 	<?php get_template_part( 'images/icons/defs/svg/sprite.defs.svg' ); ?>
 </div>
 <?php wp_footer(); ?>
+
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-25912235-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-25912235-1');
+</script>
+
 </body>
 </html>
