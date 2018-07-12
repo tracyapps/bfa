634add7af6e26e956e0726c93ac73eb5278a533b
tapps <t@thisroad.org>
2018-07-11 22:50:11 -0500

    remove hard coded analytics code (plugin reinstalled and re set up)

--- footer.php
+++ footer.php
@@ -19,15 +19,5 @@
 </div>
 <?php wp_footer(); ?>
 
-<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-25912235-1"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'UA-25912235-1');
-</script>
-
 </body>
 </html>
